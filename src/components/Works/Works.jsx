160fd4be90ad967896c7d92154681d6296a39312
Rohit22-dev <rkflash22@gmail.com>
2022-09-15 01:07:17 +0530

    Added source code

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Works.css";
 import work1 from "../../img/work-1.png";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Works = () => {
   return (
     <div className="work col-12 mx-auto">
-        <h1>Work</h1>
+      <h1>Work</h1>
+      <span>Youtube Clone</span>
       <img src={work1} alt="" />
+      <a
+        href="http://www.github.com/Rohit22-dev/Youtube-clone"
+        className="w-btn"
+      >
+        Source code <AiOutlineArrowRight className="w-arrow"/>
+      </a>
     </div>
   );
 };
